b3d657ce53e9c64e3226011c32a1f2ac210aa9a1
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-22 13:16:28 +0800

    wip

--- routes/channels.php
+++ routes/channels.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.*', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
-// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+// Broadcast::channel('App.Models.User.*', function ($user, $id) {
 //     return (int) $user->id === (int) $id;
 // });
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
 Broadcast::channel('test-channel', function () {
     return true;
 });
