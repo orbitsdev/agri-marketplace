e5b22b3dc9593969b53c3ec102f68a541e2fa9b7
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-22 12:53:21 +0800

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,15 @@
-import './bootstrap';
+import './bootstrap'; // Load Bootstrap setup
 
+import Echo from 'laravel-echo'; // Import Laravel Echo
+import Pusher from 'pusher-js'; // Import Pusher
+
+// Attach Pusher to the global window object
 window.Pusher = Pusher;
 
+// Configure Laravel Echo
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    key: import.meta.env.VITE_PUSHER_APP_KEY, // Get the key from environment variables
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Get the cluster from environment variables
+    forceTLS: true, // Use HTTPS
 });
