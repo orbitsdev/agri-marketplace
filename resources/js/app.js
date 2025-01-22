f281fde8550d4fd2a287d5992d70124a0644be21
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-22 13:07:52 +0800

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,23 @@ import Pusher from 'pusher-js'; // Import Pusher
 window.Pusher = Pusher;
 
 // Configure Laravel Echo
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY, // Get the key from environment variables
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Get the cluster from environment variables
+//     forceTLS: true, // Use HTTPS
+// });
+
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY, // Get the key from environment variables
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Get the cluster from environment variables
-    forceTLS: true, // Use HTTPS
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    authEndpoint: '/broadcasting/auth',
+    auth: {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}` // If you're using token-based authentication
+        }
+    }
 });
