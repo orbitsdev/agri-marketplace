e5159b0965a71499f1a61699e908b70573c5cbaf
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-03-19 22:00:11 +0800

    test

--- app/Filament/Resources/FarmerResource/Pages/ListFarmers.php
+++ app/Filament/Resources/FarmerResource/Pages/ListFarmers.php
@@ -13,7 +13,7 @@ class ListFarmers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
