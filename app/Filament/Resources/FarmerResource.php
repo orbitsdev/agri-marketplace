600ace200cc8a6e668d0d35fd43a77f52ee4ae4e
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-03-20 16:42:14 +0800

    wip

--- app/Filament/Resources/FarmerResource.php
+++ app/Filament/Resources/FarmerResource.php
@@ -138,7 +138,7 @@ class FarmerResource extends Resource
 
                     })->label('Farm Owner'),
                 TextColumn::make('user.email')
-                    ->searchable()->label('Email'),
+                    ->searchable()->label('Email')->copyable(),
                 TextColumn::make('farm_name')
                     ->searchable(),
                 TextColumn::make('location')
